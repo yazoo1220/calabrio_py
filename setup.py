0f4df6b7700b2dcfe1e1f26ed7b2a668a8575c6e
yazoo1220 <podman1220@hotmail.com>
2023-08-24 14:40:43 +0300

    setup for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calabrio_py',
+    version='0.1.0',
+    description='A Python client library for accessing the Calabrio API.',
+    author='yachimat',
+    author_email='podman1220@hotmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'aiohttp'
+    ],
+)
