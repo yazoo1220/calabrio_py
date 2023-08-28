0d75973a5de3ad98023c53ae447f39b106e6d7b8
yazoo1220 <podman1220@hotmail.com>
2023-08-28 16:11:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='yachimat',
     author_email='podman1220@hotmail.com',
     packages=find_packages(),
-    long_description_content_type='text/x-rst',
+    long_description='',
     install_requires=[
         'requests>=2.28.1',
         'aiohttp>=3.8.3',
