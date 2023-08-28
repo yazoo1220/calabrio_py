2e9b167bd12fe1dee02a1a5da4ac9d832bb8f81f
yazoo1220 <podman1220@hotmail.com>
2023-08-28 15:40:36 +0300

    updated dependencies to solve install problem

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='calabrio_py',
-    version='0.1.1',
+    version='0.1.2',
     description='A Python client library for accessing the Calabrio API.',
     author='yachimat',
     author_email='podman1220@hotmail.com',
     packages=find_packages(),
     install_requires=[
-        'requests',
-        'aiohttp'
+        'requests>=2.28.1',
+        'aiohttp>=3.8.3',
+        'python-dateutil>=2.8.2'
     ],
 )
