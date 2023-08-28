8e80281ae41d890477b57dda3cd99f67553e6213
yazoo1220 <podman1220@hotmail.com>
2023-08-28 16:08:01 +0300

    changed long-description setting

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='calabrio_py',
-    version='0.1.2',
+    version='0.1.3',
     description='A Python client library for accessing the Calabrio API.',
     author='yachimat',
     author_email='podman1220@hotmail.com',
     packages=find_packages(),
+    long_description_content_type='text/x-rst',
     install_requires=[
         'requests>=2.28.1',
         'aiohttp>=3.8.3',
