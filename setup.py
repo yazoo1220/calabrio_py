b3c566783641541e97308a6daec184371292d98f
yazoo1220 <podman1220@hotmail.com>
2023-10-18 10:05:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='calabrio_py',
-    version='0.1.30',
+    version='0.1.31',
     description='A Python client library for accessing the Calabrio API.',
     author='yachimat',
     author_email='podman1220@hotmail.com',
